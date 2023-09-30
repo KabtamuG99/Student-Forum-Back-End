d655e935a2ffbfbf92d17cd939494e0f3f1c6829
habtamu G <wubatg249@gmail.com>
2023-09-30 17:24:42 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const pool = require("./server/config/database");
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT||80;
+const userRouter = require('./server/api/users/user.router');
+const questionRouter = require("./server/api/questions/question.router");
+const answerRouter = require("./server/api/answers/answer.router");
+
+
+
+
+app.use(cors());
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use("/api/users", userRouter);
+app.use("/api/questions", questionRouter);
+app.use("/api/answer", answerRouter);
+
+
+
+
+
+app.listen(port,"0.0.0.0",() => console.log(`listing at http://localhost:${port}`));
